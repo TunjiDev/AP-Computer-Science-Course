543da762df7c6bb464077bdafd6c01a915674eaa
Adetunji Igbatayo <adetunjiigbatayo@gmail.com>
2025-01-16 20:05:56 +0100

    complete loop2

--- Module-1-Introduction-to-Java/loops-i/Activity1/Main.java
+++ Module-1-Introduction-to-Java/loops-i/Activity1/Main.java
@@ -2,20 +2,20 @@ package Activity1;
 
 // PRINT THE FIRST 10 NATURAL NUMBERS USING A FOR LOOP
 
-public class Main {
-   public static void main(String[] args) {
-      for (int i = 10; i >= 1; i--) {
-         System.out.println("The value of i is: " + i);
-      }
-   }
-}
-
 // public class Main {
-// public static void main(String[] args) {
-// int i = 1;
-// while (i <= 10) {
-// System.out.println(i);
-// i++;
+//    public static void main(String[] args) {
+//       for (int i = 10; i >= 1; i--) {
+//          System.out.println("The value of i is: " + i);
+//       }
+//    }
 // }
-// }
-// }
+
+public class Main {
+public static void main(String[] args) {
+int i = 1;
+while (i <= 10) {
+System.out.println(i);
+i++;
+}
+}
+}
